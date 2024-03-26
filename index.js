ddc6239103693e789b5982ab6d1f4e656c86e292
bobjoan <bobjoan966@gmail.com>
2024-03-26 15:12:53 +0100

    updated file

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function increment() {
   counter++;
   counterEl.innerText = counter;
 }
+
+let saveEl = document.getElementById("paragraph");
+saveButton.addEventListener("click", save);
+
+function save() {
+  let saver = counter + " - ";
+  saveEl.textContent += saver;
+  counterEl.textContent = 0;
+  counter = 0;
+}
